856feb3dc1ac52de145fbc6eebe571d5026a370e
PCissewski <cissek99@gmail.com>
2021-11-23 14:53:39 +0100

    add FileReader

---
+++ Projekt1/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Projekt1;
+
+    public class FileReader
+    {
+        public void Read(string path)
+        {
+            var files = Directory.GetFiles(path);
+            Array.ForEach(files,Console.WriteLine);
+        }
+        
+        private static string FetchRecord(string file)
+        {
+            
+            return file;
+        }
+    }
--- Projekt1/Program.cs
+++ Projekt1/Program.cs
@@ -1,5 +1,15 @@
 // See https://aka.ms/new-console-template for more information
 
 using System;
+using Projekt1;
 
-Console.WriteLine("Hello, World!");
+namespace Projekt1;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new FileReader();
+            reader.Read(args[0]);
+        }
+    }
